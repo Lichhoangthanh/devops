c4d57607f1ae50493c4359e416fd17accf2ccbc3
Lichhoangthanh <lichhoangthanh1204@gmail.com>
2023-12-16 02:47:00 +0300

    test dev

--- DevopsProgect/journal/app/app.py
+++ DevopsProgect/journal/app/app.py
@@ -3,7 +3,7 @@ from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
-#lichtesttt brand 2
+#lichtesttt brand 3
 app = Flask(__name__)
 application = app
 
