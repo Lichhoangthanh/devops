0eebfb4161f9d576b78fce2b60be6b8cf3d3bc22
Lichhoangthanh <lichhoangthanh1204@gmail.com>
2023-12-22 09:51:38 -0500

    update test

--- DevopsProgect/journal/test_*.py
+++ DevopsProgect/journal/test_*.py
@@ -1,10 +1,15 @@
 # /path/to/your/project/tests/test_student.py
 
-from app.app import db, Student, app
+from app.app import db, app
+from app.models import Student
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://testuser:testpassword@localhost/testdbname'
 
 def test_create_student():
-    # Tạo ngữ cảnh ứng dụng
+    # Kiểm tra và tạo cơ sở dữ liệu nếu cần
     with app.app_context():
+        db.create_all()
+
         student = Student(lastname='Doe', name='John', surname='Smith', admission_year=2022,
                           education_form='Full-time', group='A1')
 
