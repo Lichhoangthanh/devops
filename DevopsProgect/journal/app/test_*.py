b95df2f65b30b444a3b4a1f167536bc2f15cbd38
Lichhoangthanh <lichhoangthanh1204@gmail.com>
2023-12-22 16:18:47 -0500

    remove error

--- DevopsProgect/journal/app/test_*.py
+++ DevopsProgect/journal/app/test_*.py
@@ -53,19 +53,34 @@ def test_create_student(app, sample_student):
         assert retrieved_student.education_form == 'Full-time'
         assert retrieved_student.group == 'A1'
 
-# # Import lớp mới từ module models
-# from models import PlanOfStudy, Gradebook
+# Import lớp mới từ module models
+from models import PlanOfStudy, Gradebook
 
-# # Thêm fixture cho một đối tượng PlanOfStudy thử nghiệm
-# @pytest.fixture
-# def sample_plan_of_study():
-#     return PlanOfStudy(
-#         speciality='Computer Science',
-#         discipline='Data Structures',
-#         semester=2,
-#         hours=60,
-#         exam_or_test='Exam'
-#     )
+# Thêm fixture cho một đối tượng PlanOfStudy thử nghiệm
+@pytest.fixture
+def sample_plan_of_study():
+    return PlanOfStudy(
+        speciality='Computer Science',
+        discipline='Data Structures',
+        semester=2,
+        hours=60,
+        exam_or_test='Exam'
+    )
+
+
+# Kiểm tra tạo đối tượng PlanOfStudy và lưu vào cơ sở dữ liệu
+def test_create_plan_of_study(app, sample_plan_of_study):
+    with app.app_context():
+        db.session.add(sample_plan_of_study)
+        db.session.commit()
+
+        retrieved_plan_of_study = PlanOfStudy.query.filter_by(discipline='Data Structures').first()
+
+        assert retrieved_plan_of_study is not None
+        assert retrieved_plan_of_study.speciality == 'Computer Science'
+        assert retrieved_plan_of_study.semester == 2
+        assert retrieved_plan_of_study.hours == 60
+        assert retrieved_plan_of_study.exam_or_test == 'Exam'
 
 # # Thêm fixture cho một đối tượng Gradebook thử nghiệm
 # @pytest.fixture
@@ -76,21 +91,6 @@ def test_create_student(app, sample_student):
 #         discipline_id=sample_plan_of_study.id,
 #         mark=85
 #     )
-
-# # Kiểm tra tạo đối tượng PlanOfStudy và lưu vào cơ sở dữ liệu
-# def test_create_plan_of_study(app, sample_plan_of_study):
-#     with app.app_context():
-#         db.session.add(sample_plan_of_study)
-#         db.session.commit()
-
-#         retrieved_plan_of_study = PlanOfStudy.query.filter_by(discipline='Data Structures').first()
-
-#         assert retrieved_plan_of_study is not None
-#         assert retrieved_plan_of_study.speciality == 'Computer Science'
-#         assert retrieved_plan_of_study.semester == 2
-#         assert retrieved_plan_of_study.hours == 60
-#         assert retrieved_plan_of_study.exam_or_test == 'Exam'
-
 # # Kiểm tra tạo đối tượng Gradebook và lưu vào cơ sở dữ liệu
 # def test_create_gradebook(app, sample_gradebook):
 #     with app.app_context():
