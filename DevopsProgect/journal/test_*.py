f78ce34e4c6a241734765d7feef8fe6ffc3b9701
Lichhoangthanh <lichhoangthanh1204@gmail.com>
2023-12-22 09:42:27 -0500

    upadte test

--- DevopsProgect/journal/test_*.py
+++ DevopsProgect/journal/test_*.py
@@ -1,30 +1,22 @@
-# test_app.py
+# /path/to/your/project/tests/test_student.py
 
-from app import app
-
-from app.app import db, Student
+from app.app import db, Student, app
 
 def test_create_student():
-    student = Student(lastname='Doe', name='John', surname='Smith', admission_year=2022,
-                      education_form='Full-time', group='A1')
+    # Tạo ngữ cảnh ứng dụng
+    with app.app_context():
+        student = Student(lastname='Doe', name='John', surname='Smith', admission_year=2022,
+                          education_form='Full-time', group='A1')
 
-    db.session.add(student)
-    db.session.commit()
+        db.session.add(student)
+        db.session.commit()
 
-    retrieved_student = Student.query.filter_by(lastname='Doe').first()
+        retrieved_student = Student.query.filter_by(lastname='Doe').first()
 
-    assert retrieved_student is not None
-    assert retrieved_student.lastname == 'Doe'
-    assert retrieved_student.name == 'John'
-    assert retrieved_student.surname == 'Smith'
-    assert retrieved_student.admission_year == 2022
-    assert retrieved_student.education_form == 'Full-time'
-    assert retrieved_student.group == 'A1'
-
-
-
-if __name__ == "__main__":
-    # Chạy các kiểm thử bằng cách sử dụng pytest
-    import pytest
-    pytest.main()
-
+        assert retrieved_student is not None
+        assert retrieved_student.lastname == 'Doe'
+        assert retrieved_student.name == 'John'
+        assert retrieved_student.surname == 'Smith'
+        assert retrieved_student.admission_year == 2022
+        assert retrieved_student.education_form == 'Full-time'
+        assert retrieved_student.group == 'A1'
